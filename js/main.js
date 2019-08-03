878a4fe4767ec03351a371522db02ec3b165b123
Asif <mhdaxif@gmail.com>
2019-08-03 20:04:51 +0500

    add froala blocks

--- js/main.js
+++ js/main.js
@@ -2,24 +2,18 @@
 const { fromEvent, from } = rxjs;
 
 // Operators
-const { map, filter, pluck, auditTime, distinctUntilChanged, switchMap, tap, take} = rxjs.operators;
+const { map, filter, pluck, debounceTime, distinctUntilChanged, switchMap, tap, take} = rxjs.operators;
 const { ajax } = rxjs.ajax;
 
-var total;
-
 var searchEle = document.querySelector("#search");
-var size = document.querySelector("#totalPick");
 
 var source = fromEvent(searchEle, "keyup");
 var sourceObs = source.pipe(
-    auditTime(2000), 
+    debounceTime(500), 
     pluck("target", "value"),
     filter((text) => {
         // clear previous search results
         document.querySelector(".main-wrapper").innerHTML = "";
-        let val = +document.querySelector("#totalPick").value;
-        total = val <= 0 ? 5 : val; 
-        
         //  if length is greater than 1 
         return text.trim().length > 1; 
     }),
@@ -27,8 +21,8 @@ var sourceObs = source.pipe(
     switchMap((searchTerm) => { 
         searchTerm = searchTerm.trim() || "mhdaxif";
         return doSearch(searchTerm);
-    }),
-    switchMap(res => from(res.items).pipe(take(total || 5))),
+    }), 
+    switchMap(res => from(res.items)),
 );
 
 sourceObs.subscribe(
@@ -41,14 +35,20 @@ function doSearch(searchTerm) {
 }
 
 function appendItem(item) {
-    let divHtml = '';
-    divHtml += `<div class="col-sm-2 custom-card">
-            <div class="card">
-                 <img class="card-img-top" src="${item.avatar_url}" alt="Card image cap">
-                 <!--  <h5 class="card-title"> ${item.login} </h5> --> 
-                 <a href="${item.html_url}" class="btn btn-block"> Profile </a>
-                 </div>
-        </div>`;
+    let divHtml = ``;
+    divHtml += `
+        <div class="col-sm-3 m-sm-auto">
+            <div class="row align-items-center">
+                <div class="col-4">
+                    <img alt="image" class="img-fluid rounded-circle" src="${item.avatar_url}">
+                </div>
+
+                <div class="col-8">
+                    <h3> <a href="https://github.com/${item.login}"> ${item.login} </a></h3>
+                </div>
+            </div>
+        </div>
+   `;
 
     document.querySelector(".main-wrapper").innerHTML += divHtml;
 }
